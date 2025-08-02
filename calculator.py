52f430ff1bf3d55f4b4dd43a54d839dfd93eab32
kyrapaim <kyrapaim@ufl.edu>
2025-08-01 23:05:04 -0400

    modified calculator p2

--- calculator.py
+++ calculator.py
@@ -1,11 +1,27 @@
-"""
-calculator.py
-- Defines functions used to create a simple calculator
+import math
 
-One function per operation, in order.
-"""
-# First example
-def add(a, b): 
-    pass
+def add(a,b):
+    return a+b
+
+def sub(a,b):
+    return
+
+def mul(a,b):
+    return a*b
+
+def div(a,b):
+    if a == 0:
+        raise ZeroDivisionError
+    return b/a
+
+def log(a,b):
+    if a <= 0:
+        raise ValueError("Log Base must be positive")
+    if b<= 0:
+        raise ValueError('Log arg must be positive')
+    return math.log(b,a)
+
+def exp(a,b):
+    return a ** b
 
 
