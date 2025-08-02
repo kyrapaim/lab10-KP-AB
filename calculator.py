2abcb26d7fc1d7fa2267b6f3993fd044b5cc4e9d
Amkia J B <hack.amkia@gmail.com>
2025-08-01 23:34:22 -0400

    modified calculator p1

--- calculator.py
+++ calculator.py
@@ -9,11 +9,6 @@ def sub(a,b):
 def mul(a,b):
     return a*b
 
-def div(a,b):
-    if a == 0:
-        raise ZeroDivisionError
-    return b/a
-
 def log(a,b):
     if a <= 0:
         raise ValueError("Log Base must be positive")
