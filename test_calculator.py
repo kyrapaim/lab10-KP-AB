8034cedfe41ed6162449c364722d5d0fcaf3d04c
kyrapaim <kyrapaim@ufl.edu>
2025-08-03 00:45:02 -0400

    p2 test calc

--- test_calculator.py
+++ test_calculator.py
@@ -21,14 +21,19 @@ class TestCalculator(unittest.TestCase):
     ##########################
 
     ######## Partner 1
-    # def test_multiply(self): # 3 assertions
-    #     fill in code
+    def test_multiply(self): # 3 assertions
+        self.assertEqual(mul(3, 4), 12)
+        self.assertEqual(mul(10, 10),100)
+        self.assertNotEqual(mul(-10, -10),-10)
 
-    # def test_divide(self): # 3 assertions
-    #     fill in code
-    # ##########################
 
-    ######## Partner 2
+
+    def test_divide(self): # 3 assertions
+        self.assertNotEqual(div(5, 2), 5)
+        self.assertEqual(div(100, 500), 5)
+        self.assertEqual(div(8, 64), 8)
+
+
     def test_divide_by_zero(self): # 1 assertion
         # fill in code
         # call division function inside, example:
@@ -52,22 +57,11 @@ class TestCalculator(unittest.TestCase):
                log(5, 0)
     
     ######## Partner 1
-    # def test_log_invalid_argument(self): # 1 assertion
-    #     # call log function inside, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #     logarithm(0, 5)
-    #     fill in code
+    def test_log_invalid_argument(self): # 1 assertion
+        with self.assertRaises(ValueError):
+            log(0, 5)
 
-    # def test_hypotenuse(self): # 3 assertions
-    #     fill in code
 
-    # def test_sqrt(self): # 3 assertions
-    #     # Test for invalid argument, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #    square_root(NUM)
-    #     # Test basic function
-    #     fill in code
-    ##########################
 
 # Do not touch this
 if __name__ == "__main__":
